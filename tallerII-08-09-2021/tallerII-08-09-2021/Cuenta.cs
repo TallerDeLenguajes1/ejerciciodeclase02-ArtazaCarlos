5726cff4da2d1d6076cc323bd9c8c219d1f884fd
Artaza_Carlos <carlos.unt08@gmail.com>
2021-09-08 17:49:35 -0300

    plantilla de clases

---
+++ tallerII-08-09-2021/tallerII-08-09-2021/Cuenta.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tallerII_08_09_2021
+{
+    enum TipoDeExtracion
+    {
+        cajeroHumano,
+        cajeroAutomatico
+    }
+    public class Cuenta
+    {
+        private int monto;
+
+        public int Monto { get => monto; set => monto = value; }
+
+        public Cuenta()
+        {
+
+        }
+        public Cuenta(int monto)
+        {
+            Monto = monto;
+        }
+
+        public void extraccionCajeroHumano(string tipo, int cantidad)
+        {
+            if (tipo == TipoDeExtracion.cajeroHumano.ToString())
+            {
+                Monto = Monto - cantidad;
+            }
+        }  
+    }
+    public class CuentaAhorro : Cuenta
+    {
+        
+        public void extraccionCajeroAutomatico(string tipo, int cantidad)
+        {
+            if (tipo == TipoDeExtracion.cajeroAutomatico.ToString())
+            {
+                
+            }
+        }
+    }
+
+    public class CuentaCorriente : Cuenta
+    {
+
+        public void extraccionCajeroAutomatico(string tipo, int cantidad)
+        {
+            if (tipo == TipoDeExtracion.cajeroAutomatico.ToString())
+            {
+                
+            }
+        }
+    }
+}
---
+++ tallerII-08-09-2021/tallerII-08-09-2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tallerII_08_09_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
